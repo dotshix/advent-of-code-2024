b1542a219a853051847027408a57c8c74ae49d43
Dario <dariosans@gmail.com>
2024-12-01 17:39:35 -0500

    Completed day01b

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod day01a;
+mod day01b;
 
 fn main() {
     let day = std::env::args().nth(1).expect("Please specify a day (e.g., `1`)");
     match day.as_str() {
         "1a" => day01a::run(),
+        "1b" => day01b::run(),
         _ => eprintln!("Day not implemented: {}", day),
     }
 }

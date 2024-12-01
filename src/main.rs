5c273ac297e3c3a2f17aa04e620ae4c422514c09
Dario <dariosans@gmail.com>
2024-12-01 16:49:22 -0500

    day01a complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod day01a;
+
 fn main() {
-    println!("Hello, world!");
+    let day = std::env::args().nth(1).expect("Please specify a day (e.g., `1`)");
+    match day.as_str() {
+        "1a" => day01a::run(),
+        _ => eprintln!("Day not implemented: {}", day),
+    }
 }

e27c67e1540f198bc7c1faa1778f9c2217b8ec7b
Dario <dariosans@gmail.com>
2024-12-02 05:01:32 -0500

    Solved day02a

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod day01a;
 mod day01b;
+mod day02a;
 
 fn main() {
     let day = std::env::args().nth(1).expect("Please specify a day (e.g., `1`)");
     match day.as_str() {
         "1a" => day01a::run(),
         "1b" => day01b::run(),
+        "2a" => day02a::run(),
         _ => eprintln!("Day not implemented: {}", day),
     }
 }

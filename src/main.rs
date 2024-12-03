bfbba0d4b8918ab5ca6ba727d2af032944717c73
Dario <dariosans@gmail.com>
2024-12-03 06:56:35 -0500

    Solved day03b, need to learn more regex

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod day01a;
 mod day01b;
 mod day02a;
 mod day02b;
+mod day03a;
+mod day03b;
 
 fn main() {
     let day = std::env::args().nth(1).expect("Please specify a day (e.g., `1`)");
@@ -10,6 +12,8 @@ fn main() {
         "1b" => day01b::run(),
         "2a" => day02a::run(),
         "2b" => day02b::run(),
+        "3a" => day03a::run(),
+        "3b" => day03b::run(),
         _ => eprintln!("Day not implemented: {}", day),
     }
 }
